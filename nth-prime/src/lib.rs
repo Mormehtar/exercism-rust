b90b938ce47d04d45f2a72aab4519bbb2f340a1e
darkrider <darkrider@yandex-team.ru>
2021-07-26 20:04:22 +0300

    nth-prime some refactoring

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -3,50 +3,89 @@ struct SievePointer {
     last_value: usize
 }
 
+struct SieveChunk {
+    sieve: Vec<bool>,
+    offset: usize
+}
+
+
+impl SieveChunk {
+    pub fn new(size: usize, offset: usize) -> SieveChunk {
+        let mut sieve: Vec<bool> = Vec::with_capacity(size);
+        sieve.resize(size, true);
+        SieveChunk {sieve, offset}
+    }
+
+    pub fn prick_number(&mut self, mut number: &mut SievePointer) {
+        loop {
+            let new_value = number.last_value + (number.number as usize);
+            let pointer = new_value - self.offset;
+            if pointer >= self.sieve.len() {
+                break;
+            }
+            self.sieve[pointer] = false;
+            number.last_value = new_value;
+        }
+    }
+
+    pub fn get_next_prime_from(&self, from: usize) -> Option<usize> {
+        let mut start: usize = 0;
+        if from > self.offset {
+            start = from - self.offset;
+        }
+        for i in start .. self.sieve.len() {
+            if self.sieve[i] {
+                return Some(i + self.offset);
+            }
+        }
+        return None;
+    }
+}
+
 pub fn nth(n: u32) -> u32 {
-    let mut found_numbers: Vec<SievePointer> = vec![
-        SievePointer {number: 2, last_value: 2},
-        SievePointer {number: 3, last_value: 3}
-    ];
-    let mut offset = 4;  // 0, 1, 2, 3
-    let chunk_size = n.next_power_of_two() as usize;
-    while found_numbers.len() <= (n as usize) {
-        found_numbers = find_chunk(found_numbers, offset, chunk_size);
+    let prime_index = n as usize;
+    let mut found_numbers: Vec<SievePointer> = Vec::with_capacity(prime_index + 1);
+    let mut offset = 2;  // 0, 1
+    let chunk_size: usize = prime_index.next_power_of_two();
+
+    while found_numbers.len() <= prime_index {
+        found_numbers = process_chunk(found_numbers, offset, chunk_size, prime_index + 1);
         offset += chunk_size;
     }
-    return found_numbers[n as usize].number;
+    return found_numbers[prime_index].number;
 }
 
-fn find_chunk(
+fn process_chunk(
     mut found_numbers: Vec<SievePointer>,
     offset: usize,
-    chunk_size: usize
+    chunk_size: usize,
+    limit: usize,
 ) -> Vec<SievePointer> {
-    let mut sieve: Vec<bool> = Vec::with_capacity(chunk_size);
-    sieve.resize(chunk_size, true);
+    let mut sieve = SieveChunk::new(chunk_size, offset);
     for prime in &mut found_numbers {
-        prick_sieve(prime, &mut sieve, offset);
+        sieve.prick_number(prime);
     }
-    for i in 0..sieve.len() {
-        if sieve[i] {
-            let mut new_prime = SievePointer {number: (i + offset) as u32, last_value: i + offset};
-            prick_sieve(&mut new_prime, &mut sieve, offset);
-            found_numbers.push(new_prime);
-        }
-    }
-    return found_numbers;
-}
-
-fn prick_sieve(mut number: &mut SievePointer, sieve: &mut Vec<bool>, offset: usize) {
     loop {
-        let new_value = number.last_value + (number.number as usize);
-        let pointer = new_value - offset;
-        if pointer >= sieve.len() {
-            return;
+        let mut last_prime: usize = 0;
+        if found_numbers.len() > 0 {
+            last_prime = (found_numbers[found_numbers.len() - 1].number + 1) as usize;
         }
-        sieve[pointer] = false;
-        number.last_value = new_value;
+        let next_prime = sieve.get_next_prime_from(last_prime);
+        match next_prime {
+            Some(number) => {
+                {
+                    let mut new_prime = SievePointer { number: (number) as u32, last_value: number };
+                    sieve.prick_number(&mut new_prime);
+                    found_numbers.push(new_prime);
+                }
+            },
+            _ => break,
+        }
+        if found_numbers.len() == limit {
+            break;
+        }
     }
+    return found_numbers
 }
 
 
@@ -70,6 +109,19 @@ mod tests {
     }
 
     #[test]
-    fn test_big_prime() { assert_eq!(nth(10000), 104743); }
+    fn test_big_prime() {
+        assert_eq!(nth(10_000), 104743);
+    }
+
+    #[test]
+    fn test_very_big_prime() {
+        assert_eq!(nth(100_000), 1299721);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_very_very_big_prime() {
+        assert_eq!(nth(1_000_000), 15485867);
+    }
 
 }
