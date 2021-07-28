175d36e87fc7880d090327111b2769aee3fdfb9d
darkrider <darkrider@yandex-team.ru>
2021-07-29 00:37:00 +0300

    grains finish

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if !(1..=64).contains(&s) {
+        panic!("Square must be between 1 and 64")
+    }
+    get_2_in_pow_of(s)
+}
+
+fn get_2_in_pow_of(s: u32) -> u64 {
+    1 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).map(square).sum()
 }
