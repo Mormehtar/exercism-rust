35e240eec6ef5fcb07ed36fa650a785ae23d24ad
darkrider <darkrider@yandex-team.ru>
2021-07-29 00:21:53 +0300

    sum-of-multiples solution

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,13 @@
+use std::collections::hash_set::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    factors
+        .iter()
+        .map(move |x| *x)
+        .filter(|x| {*x != 0 as u32})
+        .map(|x| (x..limit).step_by(x as usize))
+        .flatten()
+        .collect::<HashSet<u32>>()
+        .iter()
+        .sum()
 }
